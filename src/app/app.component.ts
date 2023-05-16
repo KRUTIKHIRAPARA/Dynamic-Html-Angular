3819ee351c39e05509432a649d76720172edabf6
KRUTIKHIRAPARA <krutik2511@gmail.com>
2023-05-16 09:33:49 +0530

    Dynamic Html Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dynamic-Components';
+
+  dynamicArray: Array<DynamicGrid> = [];  
+  newDynamic: any = {};  
+  ngOnInit(): void {  
+      this.newDynamic = {title1: "", title2: "",title3:""};  
+      this.dynamicArray.push(this.newDynamic);  
+  }  
+  
+  addRow() {    
+      this.newDynamic = {title1: "", title2: "",title3:""};  
+      this.dynamicArray.push(this.newDynamic); 
+      console.log('New row added successfully');
+      console.log(this.dynamicArray);  
+      return true;  
+    }  
+    
+    deleteRow(index:any) {  
+      if(this.dynamicArray.length ==1) {  
+        console.log('Cant delete the row when there is only one row');  
+          return false;  
+      } else {  
+          this.dynamicArray.splice(index, 1); 
+          console.log('Row deleted successfully');
+          return true;  
+      }  
+  }  
+
+}
+
+export class DynamicGrid{     
+  title1?:string;  
+  title2?:string;  
+  title3?:string;  
 }
